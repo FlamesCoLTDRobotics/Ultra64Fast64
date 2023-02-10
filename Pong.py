007d4ea6cc6223776327af4f58150ab6fe999dc0
Flames Co. Robotics <113800788+FlamesCoLTDRobotics@users.noreply.github.com>
2023-02-09 16:55:04 -0800

    [Request to upload 1.0] - 1.9.20XX

---
+++ Pong.py
@@ -0,0 +1,93 @@
+import turtle
+
+# Set window size and title
+turtle.setup(width=800, height=600)
+turtle.title('Pong')
+
+# Create two paddles
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape('square')
+paddle_a.color('white')
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape('square')
+paddle_b.color('white')
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Create a ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('circle')
+ball.color('white')
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.2
+ball.dy = 0.2
+
+# Create a scoring system
+score_a = 0
+score_b = 0
+
+# Create a pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write('Player A: 0 Player B: 0', align='center', font=('Courier', 24, 'normal'))
+
+# Disable the maximize button
+turtle.resizable(False, False)
+
+# Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard bindings
+turtle.listen()
+turtle.onkeypress(paddle_a_up, 'w')
+turtle.onkeypress(paddle_a_down, 's')
+turtle.onkeypress(paddle_b_up, 'Up')
+turtle.onkeypress(paddle_b_down, 'Down')
+
+# Main game loop
+while True:
+    turtle.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
